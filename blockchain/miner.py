9c9e30fbec08ba52259f9737a0cc46dbf40c02ee
Diana Ignatescu <diana.ignatescu8@gmail.com>
2019-07-12 17:42:59 +0100

    Fix incorrect logic

--- blockchain/miner.py
+++ blockchain/miner.py
@@ -24,7 +24,7 @@ def proof_of_work(last_proof):
     print("Searching for next proof")
     proof = 0
 
-    while valid_proof(last_proof, proof):
+    while not valid_proof(last_hash, proof):
         proof += 1
 
     print("Proof found: " + str(proof) + " in " + str(timer() - start))
